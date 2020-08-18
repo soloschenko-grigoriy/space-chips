3e0d91f64bcb5ee546e6eade6b47d66fba6c88c6
Greg Solo <gregory.soloshchenko@gmail.com>
2020-08-18 19:44:35 -0400

    Board and Node

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -2,5 +2,48 @@
 
 public class Board : MonoBehaviour
 {
-  private 
+    [SerializeField]
+    private GameObject _nodePrefab = default;
+
+    [SerializeField, Range(1, 10)]
+    private int _size = default;
+
+    private Node[] _nodes;
+
+    void Start()
+    {
+        _nodes = new Node[_size * _size];
+
+        for (int z = 0, i = 0; z < _size; z++)
+        {
+            for (int x = 0; x < _size; x++, i++)
+            {
+                var node = Instantiate(_nodePrefab);
+                node.transform.parent = transform;
+                node.transform.localPosition = new Vector3(x, 0, z);
+
+                _nodes[i] = node.GetComponent<Node>();
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                for (int i = 0; i < _nodes.Length; i++)
+                {
+                    if (hit.transform == _nodes[i].transform)
+                    {
+                        _nodes[i].Click();
+                    }
+                }
+            }
+        }
+    }
 }
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    private bool _clicked = false;
+
+    public void Click()
+    {
+        _clicked = true;
+    }
+
+    void OnDrawGizmos()
+    {
+        if (!_clicked)
+        {
+            return;
+        }
+
+        Gizmos.DrawWireCube(transform.position, new Vector3(1, 0.001f, 1));
+    }
+}
