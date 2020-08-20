d54f60b1fce44200f02f70e27f3b4464ba07d47c
Greg Solo <gregory.soloshchenko@gmail.com>
2020-08-19 22:42:23 -0400

    Simplify board and fleet

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -3,29 +3,7 @@
 public class Board : MonoBehaviour
 {
     [SerializeField]
-    private GameObject _nodePrefab = default;
-
-    [SerializeField, Range(1, 10)]
-    private int _size = default;
-
-    private Node[] _nodes;
-
-    void Start()
-    {
-        _nodes = new Node[_size * _size];
-
-        for (int z = 0, i = 0; z < _size; z++)
-        {
-            for (int x = 0; x < _size; x++, i++)
-            {
-                var node = Instantiate(_nodePrefab);
-                node.transform.parent = transform;
-                node.transform.localPosition = new Vector3(x, 0, z);
-
-                _nodes[i] = node.GetComponent<Node>();
-            }
-        }
-    }
+    public Node[] Nodes;
 
     void Update()
     {
@@ -36,11 +14,11 @@ public class Board : MonoBehaviour
 
             if (Physics.Raycast(ray, out hit))
             {
-                for (int i = 0; i < _nodes.Length; i++)
+                for (int i = 0; i < Nodes.Length; i++)
                 {
-                    if (hit.transform == _nodes[i].transform)
+                    if (hit.transform == Nodes[i].transform)
                     {
-                        _nodes[i].ToggleActive();
+                        Nodes[i].ToggleActive();
                     }
                 }
             }
